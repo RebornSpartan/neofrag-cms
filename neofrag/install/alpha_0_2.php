0934c2771acb929d97dd6a1722394b38eadff66e
Michaël BILCOT <foxley@neofrag.fr>
2017-11-06 20:27:07 +0100

    patch 0.2

---
+++ neofrag/install/alpha_0_2.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * https://neofr.ag
+ * @author: Michaël BILCOT <michael.bilcot@neofr.ag>
+ */
+
+namespace NF\NeoFrag\Install;
+
+use NF\NeoFrag\Loadables\Install;
+
+class Alpha_0_2 extends Install
+{
+	public function up()
+	{
+	}
+}
