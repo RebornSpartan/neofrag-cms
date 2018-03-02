NeoFrag/NeoFrag

[Core][Url] Global improving + language can be passed by url

modules/forum/controllers/index.php
@@ -72,7 +72,7 @@ class Index extends Controller_Module
 								'topics' => $topics
 							]), FALSE);
 
-		$content = '<a class="btn btn-default pull-left" href="'.url(($this->session->get_back() ?: 'forum')).'">'.$this->lang('back').'</a>';
+		$content = '<a class="btn btn-default pull-left" href="'.url(($this->url->back() ?: 'forum')).'">'.$this->lang('back').'</a>';
 
 		if ($pagination = $this->pagination->get_pagination())
 		{
@@ -214,7 +214,7 @@ class Index extends Controller_Module
 			}
 		}
 
-		$content = '<a class="btn btn-default pull-left" href="'.url($this->session->get_back() ?: 'forum/'.$forum_id.'/'.url_title($forum_title)).'">'.$this->lang('back').'</a>';
+		$content = '<a class="btn btn-default pull-left" href="'.url($this->url->back() ?: 'forum/'.$forum_id.'/'.url_title($forum_title)).'">'.$this->lang('back').'</a>';
 
 		if ($pagination = $this->pagination->get_pagination())
 		{

modules/forum/views/new.tpl.php
@@ -20,10 +20,10 @@
 					</div>
 					<?php endif ?>
 					<?php if (!empty($forum_id)): ?>
-					<a href="<?php echo url($this->session->get_back() ?: 'forum/'.$forum_id.'/'.url_title($title)) ?>" class="btn btn-default"><?php echo $this->lang('back') ?></a>
+					<a href="<?php echo url($this->url->back() ?: 'forum/'.$forum_id.'/'.url_title($title)) ?>" class="btn btn-default"><?php echo $this->lang('back') ?></a>
 					<button type="submit" class="btn btn-primary"><?php echo $this->lang('post_topic') ?></button>
 					<?php elseif (!empty($topic_id)): ?>
-					<a href="<?php echo url($this->session->get_back() ?: 'forum/topic/'.$topic_id.'/'.url_title($title)) ?>" class="btn btn-default"><?php echo $this->lang('back') ?></a>
+					<a href="<?php echo url($this->url->back() ?: 'forum/topic/'.$topic_id.'/'.url_title($title)) ?>" class="btn btn-default"><?php echo $this->lang('back') ?></a>
 					<button type="submit" class="btn btn-primary"><?php echo $this->lang($is_topic ? 'modify_topic' : 'modify_message') ?></button>
 					<?php else: ?>
 					<button type="submit" class="btn btn-primary"><?php echo $this->lang('reply_topic') ?></button>

RebornSpartan/neofrag-cms

modules/forum/controllers/index.php
@@ -72,7 +72,7 @@ class Index extends Controller_Module
 								'topics' => $topics
 							]), FALSE);
 
-		$content = '<a class="btn btn-default pull-left" href="'.url(($this->session->get_back() ?: 'forum')).'">'.$this->lang('Retour').'</a>';
+		$content = '<a class="btn btn-default pull-left" href="'.url(($this->url->back() ?: 'forum')).'">'.$this->lang('Retour').'</a>';
 
 		if ($pagination = $this->module->pagination->get_pagination())
 		{
@@ -214,7 +214,7 @@ class Index extends Controller_Module
 			}
 		}
 
-		$content = '<a class="btn btn-default pull-left" href="'.url($this->session->get_back() ?: 'forum/'.$forum_id.'/'.url_title($forum_title)).'">'.$this->lang('Retour').'</a>';
+		$content = '<a class="btn btn-default pull-left" href="'.url($this->url->back() ?: 'forum/'.$forum_id.'/'.url_title($forum_title)).'">'.$this->lang('Retour').'</a>';
 
 		if ($pagination = $this->module->pagination->get_pagination())
 		{

modules/forum/views/new.tpl.php
@@ -20,10 +20,10 @@
 					</div>
 					<?php endif ?>
 					<?php if (!empty($forum_id)): ?>
-					<a href="<?php echo url($this->session->get_back() ?: 'forum/'.$forum_id.'/'.url_title($title)) ?>" class="btn btn-default"><?php echo $this->lang('Retour') ?></a>
+					<a href="<?php echo url($this->url->back() ?: 'forum/'.$forum_id.'/'.url_title($title)) ?>" class="btn btn-default"><?php echo $this->lang('Retour') ?></a>
 					<button type="submit" class="btn btn-primary"><?php echo $this->lang('Poster le sujet') ?></button>
 					<?php elseif (!empty($topic_id)): ?>
-					<a href="<?php echo url($this->session->get_back() ?: 'forum/topic/'.$topic_id.'/'.url_title($title)) ?>" class="btn btn-default"><?php echo $this->lang('Retour') ?></a>
+					<a href="<?php echo url($this->url->back() ?: 'forum/topic/'.$topic_id.'/'.url_title($title)) ?>" class="btn btn-default"><?php echo $this->lang('Retour') ?></a>
 					<button type="submit" class="btn btn-primary"><?php echo $this->lang($is_topic ? 'modify_topic' : 'modify_message') ?></button>
 					<?php else: ?>
 					<button type="submit" class="btn btn-primary"><?php echo $this->lang('Répondre au sujet') ?></button>